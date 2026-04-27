1461c20e4c3abbd69f349d77ae1ecdbcdf9334a0
siamMR001 <shariful.islam.tanvir13@gmail.com>
2026-04-27 22:12:59 +0600

    Fix: specify absolute path for .env in index.js to prevent startup crashes

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,8 @@ const cookieParser = require('cookie-parser');
 const dns = require('dns');
 dns.setDefaultResultOrder('ipv4first');
 dns.setServers(['8.8.8.8']);
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 
 
 
@@ -75,7 +76,6 @@ app.use(cors({
     credentials: true
 }));
 app.use(cookieParser());
-const path = require('path');
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Basic Route for testing
